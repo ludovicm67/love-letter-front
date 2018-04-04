f9f937c459cfff2099a4b9bad785f6a0a513dd73
Unknown <morganeritter@hotmail.fr>
2018-04-04 21:34:08 +0200

    JoinGameStyle almost finish

--- src/scenes/MenuContainer/components/JoinGame/index.js
+++ src/scenes/MenuContainer/components/JoinGame/index.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
-import { API_URL, echo } from '../../../../utils';
+import { API_URL, echo, colors } from '../../../../utils';
+
+//import '../../../../node_modules/font-awesome/css/font-awesome.min.css';
+
 
 export default class JoinGame extends Component {
   constructor(props) {
@@ -57,18 +60,44 @@ export default class JoinGame extends Component {
   }
 
   render() {
+    var joinGameStyle = {
+      fontSize: '2em',
+      lineHeight: '1.5',
+      textColor: colors.blackColor,
+
+      title: {
+        fontSize: '1.9em',
+        textAlign: 'center'
+      },
+
+      buttonStyle:{
+        backgroundColor: colors.darkMainColor,
+        border: 'none',
+        fontSize: '1em'
+      },
+
+      cellule:{
+        padding:'2.5vh'
+      },
+
+      play:{
+          textAlign: 'left',
+          marginLeft: '5vh',
+          padding: '5vh'
+      }
+
+    };
     const games = this.state.games.map(game => {
       let deleteAction;
-      let joinAction = <Link to="/jeu"><button>Rejoindre</button></Link>;
+      let joinAction = <Link to="/jeu"><button style={joinGameStyle.buttonStyle}>Rejoindre</button></Link>;
       if (game.creator.name === localStorage.name) {
-        deleteAction = <button onClick={this.deleteGame.bind(this, game.id)}>Supprimer</button>;
+        deleteAction = <button style={joinGameStyle.buttonStyle} onClick={this.deleteGame.bind(this, game.id)}>Supprimer</button>;
       }
-      return <li key={game.id}>Partie de {game.creator.name}{joinAction}{deleteAction}</li>
+      return <tr key={game.id}><td style={joinGameStyle.cellule}>Salon</td><td style={joinGameStyle.cellule}>{game.creator.name}</td><td style={joinGameStyle.cellule}></td><td style={joinGameStyle.cellule}></td><td style={joinGameStyle.cellule}></td><td style={joinGameStyle.cellule}>{joinAction}</td><td style={joinGameStyle.cellule}>{deleteAction}</td></tr>
     });
-
     return (
-      <div>
-        <h1>
+      <div style={joinGameStyle}>
+        <h1 style={joinGameStyle.title}>
           <FormattedMessage id="JoinGame.title" />
         </h1>
 
@@ -80,7 +109,18 @@ export default class JoinGame extends Component {
           <FormattedMessage id="JoinGame.backToMenu" />
         </Link>
 
-        <ul>{games}</ul>
+        <table style={joinGameStyle.play}>
+          <tr>
+            <th style={joinGameStyle.cellule}></th>
+            <th style={joinGameStyle.cellule}>Joueur 1</th>
+            <th style={joinGameStyle.cellule}>Joueur 2</th>
+            <th style={joinGameStyle.cellule}>Joueur 3</th>
+            <th style={joinGameStyle.cellule}>Joueur 4</th>
+            <th style={joinGameStyle.cellule}></th>
+            <th style={joinGameStyle.cellule}></th>
+          </tr>
+          {games}
+        </table>
       </div>
     );
   }

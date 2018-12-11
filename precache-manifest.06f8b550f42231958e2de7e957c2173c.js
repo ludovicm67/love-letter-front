c4461e95bc2dfa2bf12d257f163fbb3b34815762
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-11 16:40:28 +0000

    Deploy love-letter.ludovic-muller.fr to github.com/ludovicm67/love-letter-front.git:gh-pages

---
+++ precache-manifest.06f8b550f42231958e2de7e957c2173c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "df91e1f1f683f7d949c2",
+    "url": "/static/css/main.8d4ef417.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b6d972d8d8eba4348d3f",
+    "url": "/static/js/1.b6d972d8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df91e1f1f683f7d949c2",
+    "url": "/static/js/main.df91e1f1.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "b6d972d8d8eba4348d3f",
+    "url": "/static/css/1.f23114c8.chunk.css"
+  },
+  {
+    "revision": "89ceb81909b9bd48febb09cf9c950fdd",
+    "url": "/index.html"
+  }
+];
